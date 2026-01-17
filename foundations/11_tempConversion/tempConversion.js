400f5de4604a521a5dbb92ef1635a0f2652b2be7
majack <majackwariaka@gmail.com>
2026-01-17 23:05:51 +0300

    function to convert temperatures to degrees celcius and another to farenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const toCelcius = (temperature - 32) * 5/9;
+  return Number(toCelcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const toFahrenheit = (9/5 * temperature) + 32;
+  return Number(toFahrenheit.toFixed(1));    
 };
+console.log(convertToFahrenheit(100));
+console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
