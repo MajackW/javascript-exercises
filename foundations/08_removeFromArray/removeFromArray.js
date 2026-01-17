20f559897ac04502b3511eb381519abac5f29b9a
majack <majackwariaka@gmail.com>
2026-01-17 21:23:55 +0300

    created a function that removes elements of an array using the filter method

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr, ...toRemove) {
+    newArray = arr.filter((n) => !toRemove.includes(n));
+    return(newArray);
+}
 // Do not edit below this line
 module.exports = removeFromArray;
